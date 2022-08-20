0f40952d5b16fa39d951d0c81fdddaeba43c9384
cschneider <cschneider143@gmail.com>
2022-08-20 14:01:02 -0500

    add prop and event binding to button

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,14 +2,35 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  templateUrl: './input-button-unit.component.html',
+  //templateUrl: './input-button-unit.component.html',
+  template: `
+  <!-- <p>
+    input-button-unit works!
+    The title is: {{ title }}
+  </p> -->
+
+  <input #inputElementRef [value] = "title" (keyup.enter)="changeTitle($event.target.value)">
+
+  <button (click)="changeTitle(inputElementRef.value)">Save</button>
+
+  `
+  ,
   styleUrls: ['./input-button-unit.component.scss']
 })
-export class InputButtonUnitComponent implements OnInit {
 
-  constructor() { }
+export class InputButtonUnitComponent implements OnInit {
+  title = 'Hello World';
+
+  constructor() {}
+
 
   ngOnInit(): void {
+
+  }
+
+  changeTitle(newTitle: string): void {
+    this.title = newTitle;
+    console.log(newTitle);
   }
 
 }
